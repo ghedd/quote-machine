ee94714b4f601b5cbd19081516396ba994997c76
eddielewis-thinhle <eddie.thinhle@outlook.com>
2020-06-25 15:57:43 -0400

    created base functions to fetch data from API(s)

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 // import logo from "./logo.svg";
 import "../style/main.scss";
 import QuoteBox from "./QuoteBox";
 import Footer from "./Footer";
 
-const author = {
+const photoAuthor = {
   name: "Danielle MacInnes",
   credit:
     "https://unsplash.com/@dsmacinnes?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText",
@@ -13,15 +13,39 @@ const author = {
   photoLib: "Unsplash",
 };
 
+const QUOTE_URL = "http://quotes.stormconsultancy.co.uk/random.json";
+
+const getQuoteContent = async (callback) => {
+  try {
+    let resp = await fetch(QUOTE_URL);
+    if (resp.ok) {
+      let data = await resp.json();
+      callback(data);
+    } else {
+      throw Error;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 const App = () => {
+  // const [loading, setLoading] = useState(true);
+  const [quoteContent, setQuoteContent] = useState([]);
+  // const [error, setError] = useState("");
+
+  useEffect(() => {
+    getQuoteContent((quoteContent) => setQuoteContent(quoteContent));
+  }, []);
+
   return (
     <main className="App">
       <QuoteBox />
       <Footer
-        photoAuthor={author.name}
-        authorCredLink={author.credit}
-        authorUnsplash={author.unspLink}
-        unsplash={author.photoLib}
+        photoAuthor={photoAuthor.name}
+        authorCredLink={photoAuthor.credit}
+        authorUnsplash={photoAuthor.unspLink}
+        unsplash={photoAuthor.photoLib}
       />
     </main>
   );
